1b5d67685ab48cbd966eb6da804214e531a95bc7
Max Cayton <caytonm5340@student.faytechcc.edu>
2023-04-04 17:27:16 -0400

    Organized GameMenu Class Library

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,5 +1,5 @@
 ﻿/**
-* 3/29/2023
+* 4/4/2023
 * CSC 153
 * Max Cayton
 * This program provides a menu that displays content from the game World of Villainy.
@@ -17,11 +17,81 @@ namespace ConsoleUI
 {
     class Program
     {
-
         static void Main(string[] args)
         {
+            char keepGoing = 'y';
+            string menuChoice;
+
+            while (keepGoing == 'y')
+            {
+                GameMenu.DisplayMenu();
+
+                // Get Input
+                menuChoice = Console.ReadLine();
+                Console.WriteLine("\n");
+
+                switch(menuChoice)
+                {
+                    case "1":
+                    case "rooms":
+
+                        GameMenu.DisplayRooms();
+
+                        break;
+
+                    case "2":
+                    case "weapons":
+
+                        GameMenu.DisplayWeapons();
+
+                        break;
+
+                    case "3":
+                    case "potions":
+
+                        GameMenu.DisplayPotions();
+
+                        break;
+
+                    case "4":
+                    case "treasure":
+
+                        GameMenu.DisplayTreasure();
+
+                        break;
+
+                    case "5":
+                    case "items":
+
+                        GameMenu.DisplayItems();
+
+                        break;
+
+                    case "6":
+                    case "mobs":
+
+                        GameMenu.DisplayMobs();
+
+                        break;
+
+                    case "7":
+                    case "fight":
+
+                        Combat.Battle();
+
+                        break;
+
+                    default:
+                        Console.WriteLine("\nThanks for playing! (Press any key to exit)");
+                        keepGoing = 'n';
+                        break;
+
+                }
+
+                Console.WriteLine("\n");
+
+            }
             
-            GameMenu.DisplayMenu();
 
             Console.ReadKey();
 
