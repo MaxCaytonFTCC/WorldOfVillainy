a1eee767128ca21cabe258564f396f3823484891
MaxCaytonFTCC <90518913+MaxCaytonFTCC@users.noreply.github.com>
2023-05-05 19:58:30 -0400

    Add files via upload

---
+++ WorldOfVillainy/GameWorldLibrary/Models/Mob.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameWorldLibrary
+{
+    public class Mob
+    {
+
+        // Default Constructor
+        public Mob() { }
+
+
+        public Mob(int id, string name, string race, string combatClass, int hp, int ac, string weapon, string description, List<Treasure> treasureInventory)
+        {
+            ID = id;
+            Name = name;
+            Race = race;
+            CombatClass = combatClass;
+            HP = hp;
+            AC = ac;
+            Weapon = weapon;
+            Description = description;
+            TreasureInventory = treasureInventory;
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Race { get; set; }
+        public string CombatClass { get; set; }
+        public int HP { get; set; }
+        public int AC { get; set; }
+        public string Weapon { get; set; }
+        public string Description { get; set; }
+        public List<Treasure> TreasureInventory { get; set; }
+
+
+    }
+}
---
+++ WorldOfVillainy/GameWorldLibrary/Models/Potion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameWorldLibrary
+{
+    public class Potion
+    {
+
+        // Default Constructor
+        public Potion() { }
+
+        public Potion(int id, string name, int price, int valueChange, string description)
+        {
+            ID = id;
+            Name = name;
+            Price = price;
+            ValueChange = valueChange;
+            Description = description;
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int ValueChange { get; set; }
+        public string Description { get; set; }
+
+    }
+}
